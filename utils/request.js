4503de8c21e941659020da3ab2d29c40652772dd
chenting <3213043406@qq.com>
2026-01-28 17:07:39 +0800

    flyio二次封装

---
+++ utils/request.js
@@ -0,0 +1,54 @@
+import fly from "flyio";
+import Fly from "flyio";
+
+const fly = new Fly();
+
+fly.config, (baseUrl = "https://www.winweb.cloud.mall");
+fly.config.timeout = 1000 * 20;
+
+// 创建一个自定义错误类
+class RequestError extends Error {
+  constructor (message, code) {
+    super(message)
+    this.code = code
+    this.data = data
+  }
+}
+
+// 对请求和响应进行拦截
+// 添加请求拦截器
+fly.interceptors.request.use((request) => {
+  // 给所有请求添加自定义header
+  // 可以显式返回request, 也可以不返回，没有返回值时拦截器中默认返回request
+  return request;
+});
+
+// 添加响应拦截器，响应拦截器会在then/catch处理之前执行
+fly.interceptors.response.use(
+  (response) => {
+    // 处理请求异常逻辑
+    const {status, data} = response
+    if (status !== 200) {
+      const message = '[Fetch]:网阔开小差'
+      return Promise.reject(new RequestError(message))
+    }
+    // 只将请求结果的data字段返回
+    return response.data;
+  },
+  (err) => {
+    // 发生网络错误后会走到这里
+    if (!err) {
+      err.message = '网络异常,请稍后再试'
+      setTimeout(() => {
+        wx.showToast({
+          title: err.message,
+          icon: "none",
+          duration: 2000
+        })
+      }, 1000)
+      return Promise.reject(err)
+    }
+  }
+);
+
+export default fly
